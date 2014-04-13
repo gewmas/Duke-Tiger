a38f392ff21433a80c77b953227c1840fc576dd7
myhgew <yuhua.mai@duke.edu>
2014-04-13 12:42:01 -0400

    change initarray with word size, pass test

--- passtest/test4.tig.s
+++ passtest/test4.tig.s
@@ -21,8 +21,8 @@ sw $a3, -16($fp)
 #body:
 lw $t1, -4($fp)
 addi $t0, $zero, 0
-beq $t1, $t0, L2061
-L2062:
+beq $t1, $t0, L348
+L349:
 lw $t0, -4($fp)
 move $s0, $t0
 #save arguments to reg
@@ -61,7 +61,7 @@ lw $t0, 56($sp)
 move $t0, $v0
 mul $t0, $s0, $t0
 move $t0, $t0
-L2063:
+L350:
 move $v0, $t0
 #load calleesaves:
 lw $s7, 52($sp)
@@ -77,10 +77,10 @@ lw $t0, 0($sp)
 move $fp, $t0
 addi $sp, $sp, 100
 jr $ra
-L2061:
+L348:
 li $t0, 1
-j L2063 
-L2065:
+j L350 
+L352:
 .text
 tig_main:
 #save calleesaves:
@@ -146,7 +146,7 @@ lw $t0, 0($sp)
 move $fp, $t0
 addi $sp, $sp, 96
 jr $ra
-L2074:
+L361:
 
 
 
--- passtest/testSL2.tig.s
+++ passtest/testSL2.tig.s
@@ -21,8 +21,8 @@ sw $a3, -16($fp)
 #body:
 lw $t1, -4($fp)
 addi $t0, $zero, 0
-beq $t1, $t0, L2135
-L2136:
+beq $t1, $t0, L422
+L423:
 lw $t0, -4($fp)
 move $s0, $t0
 #save arguments to reg
@@ -61,7 +61,7 @@ lw $t0, 56($sp)
 move $t0, $v0
 mul $t0, $s0, $t0
 move $t0, $t0
-L2137:
+L424:
 move $v0, $t0
 #load calleesaves:
 lw $s7, 52($sp)
@@ -77,13 +77,13 @@ lw $t0, 0($sp)
 move $fp, $t0
 addi $sp, $sp, 100
 jr $ra
-L2135:
+L422:
 lw $t0, -100($fp)
 lw $t0, -4($t0)
 addi $t0, $t0, 1
 move $t0, $t0
-j L2137 
-L2139:
+j L424 
+L426:
 .text
 tig_main:
 #save calleesaves:
@@ -152,7 +152,7 @@ lw $t0, 0($sp)
 move $fp, $t0
 addi $sp, $sp, 100
 jr $ra
-L2148:
+L435:
 
 
 
